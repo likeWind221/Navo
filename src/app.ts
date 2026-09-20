a55d5011815fb745b033cfc3d2e299be1d37b701
Zhanshuo Cao <118382186+likeWind221@users.noreply.github.com>
2026-09-20 16:57:15 +0800

    F9.6d.2: mount Resource Service

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import type { NodePluginConfig } from "./node/plugin.js";
 import { NodePlugin } from "./node/plugin.js";
 import { resolveAgentBinding } from "./project/binding.js";
 import { MainSessionService } from "./project/session.js";
+import { ResourceService } from "./resource/service.js";
 import { ResourceStore } from "./resource/store.js";
 import { SessionStore } from "./session/store.js";
 import { ProjectStore } from "./project/store.js";
@@ -70,6 +71,7 @@ export async function NavoApp(
   await ctx.plugin(NodePlugin, config.node);
   await ctx.plugin(MailboxStore);
   await ctx.plugin(ResourceStore);
+  await ctx.plugin(ResourceService);
   await ctx.plugin(MainSessionService, { model: config.node.session.model });
   await ctx.plugin(RoadmapStore);
   await ctx.plugin(RoadmapToolsPlugin);
